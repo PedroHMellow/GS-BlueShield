3d1cd6b2f0c3140ffa1bc59ae9163a9d841a5b4b
PedroHMello <israelalvesmello2003@gmail.com>
2024-06-06 19:05:22 -0300

    add V1.0

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -5,12 +5,14 @@ import Header from "../../Components/Header/Header"
 function Dashboard () {
     return (
         <>
-            <section className="bg-Azul_Fiap w-full h-screen">
+            <section style={{backgroundImage: "url('Banner.png')"}} className="min-w-full">
                 <Header /> 
 
                 <div className="flex p-24">
                     <h1 className="text-white text-5xl"> Dashboard </h1>
                 </div>
+                
+                <div className="p-12"></div>
                 
                 <Footer />
             </section>
